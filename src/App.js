eceda12668550e0a78cf64563d0c703d9a078c77
Shafin <shafinbinshakeel@gmail.com>
2022-01-01 23:23:24 +0530

    Project init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Coding from "./Components/coding.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Coding />
+    </div>
+  );
+}
+
+export default App;
